f57cb5aa277ab59ca66d74eacae8bc599a8e2622
Till Brügmann <till.bruegmann@icloud.com>
2024-11-30 11:38:59 +0100

    opt

--- Productivity Pro/Views/ContentView.swift
+++ Productivity Pro/Views/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
             } label: {
                 Image(systemName: "checklist")
             }
+            .badge(tasks.count)
 
             Tab {
                 ScheduleViewContainer()
